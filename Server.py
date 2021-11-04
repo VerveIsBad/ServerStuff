1b959476999b406e27cc0eef67dba1797141b6eb
Christopher Truluck <ctruluck12107@gmail.com>
2021-11-04 21:06:16 +0000

    .

--- Server.py
+++ Server.py
@@ -1,66 +1,32 @@
-import socket 
-import threading
+def find_ip(Mlength):
+    ip = []
+    total = []
+    with open(file, 'r') as f:
+        for line in f:
+            for i in range(Mlength):
+                if line[i] == '-' or line[i] == ' ':
+                    pass
+                else:
+                    ip.append(line[i])
+            if ip not in total:
+                total.append(ip)
+            ip = []
+    
+    return len(ip)
 
 
-HEADER = 64
-PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
-# Want to be non-local? Change to public IP adress ^^^
-ADDR = (SERVER, PORT)
-FORMAT = 'utf-8'
-DISCCONECT_MESSAGE = "!DISCONNECT"
-
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
-
-
-
-def handle_client(conn, addr):
-
-    # Shows client IP
-    print(f"[NEW CONNECTION] {addr} connected.")
-
-    # Starts the loop; this happens after the thread starts this function.
-    connected = True 
-    while connected:
-        """
-        Message is received in bytes, so we decode this and convert it into the length of the message
-        Server: receives bytes message
-        Message: '(message length) 7 (Insert spaces for padding)'
-        """
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        # Does message actually contain text / real message?
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            # Checks if the client has sent the, "Disconnect message"
-            if msg == DISCCONECT_MESSAGE:
-                connected = False  
+def find_in_file(to_find, file, prnt_line = False):
+    total = 0
+    with open(file, 'r') as f:
+        for line in f:
+            if to_find in line:
+                total += 1
+                if prnt_line == True:
+                    print(line)
+            else:
+                pass
         
-            print(f"[{addr}] {msg}")
-            '''
-            Server to client messaging:
-
-            conn.send('MEssage stuHEhehepodv'.encode(FORMAT))
-            '''
-    
-    conn.close()
+    return total 
 
 
-def start():
-    server.listen()
-    # Prints the server IP
-    print(f"[LISTENING] server is linstening on: {SERVER}")
-    
-    while True:
-        conn, addr = server.accept()
-        # directs a thread to the target function, and shows it's arguemnts.
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
-
-# Starts server
-print("[SERVER IS STARTING . . .]")
-start()
-
+print(find_in_file("\x04\x01\x00P\xC6\xCE\x0Eu0\x00", 'access.log', True))
